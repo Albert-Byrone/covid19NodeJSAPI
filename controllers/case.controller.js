9990b463c37871ac657025e190ba75901468f60e
albertbyrone <albertbyrone16777@gmail.com>
2021-01-08 11:46:35 +0300

    update case

--- controllers/case.controller.js
+++ controllers/case.controller.js
@@ -1,21 +1,35 @@
-// Find a single note with a noteId
-exports.findOne = (req, res) => {
-    Case.findById(req.params.id)
-    .then(cases => {
-        if(!cases) {
+// Update a note identified by the noteId in the request
+exports.update = (req, res) => {
+    // Validate Request
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    // Find note and update it with the request body
+    Case.findByIdAndUpdate(req.params.id, {
+        date: req.body.date,
+        state: req.body.state,
+        country: req.body.country,
+        deaths: req.body.deaths,
+        cases: req.body.cases,
+    }, {new: true})
+    .then(casee => {
+        if(!casee) {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.id
-            });            
+                message: "Case not found with id " + req.params.id
+            });
         }
-        res.send(cases);
+        res.send(casee);
     }).catch(err => {
         if(err.kind === 'ObjectId') {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.id
+                message: "Note not found with id " + req.params.noteId
             });                
         }
         return res.status(500).send({
-            message: "Error retrieving note with id " + req.params.id
+            message: "Error updating note with id " + req.params.noteId
         });
     });
 };
