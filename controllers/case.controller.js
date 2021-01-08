bb5f74b410f8b7033892e9d2308975aaee127ef6
albertbyrone <albertbyrone16777@gmail.com>
2021-01-08 11:46:03 +0300

    get all the cases

--- controllers/case.controller.js
+++ controllers/case.controller.js
@@ -1,57 +1,13 @@
-exports.create = (req, res) => {
-    // Validate request
-    console.log(req.body);
-    if(!req.body.date) {
-        return res.status(400).send({
-            message: "Date ontent can not be empty"
-        });
-    }
-
-    if(!req.body.state) {
-        return res.status(400).send({
-            message: "State content can not be empty"
-        });
-    }
-
-
-    if(!req.body.country) {
-        return res.status(400).send({
-            message: "Country content can not be empty"
-        });
-    }
-
-
-    if(!req.body.deaths) {
-        return res.status(400).send({
-            message: "Deaths content can not be empty"
-        });
-    }
-
-
-    if(!req.body.cases) {
-        return res.status(400).send({
-            message: "Cases content can not be empty"
-        });
-    }
-
-
-    // Create a Note
-    const casee = new Case({
-        date: req.body.date,
-        country: req.body.country, 
-        state: req.body.state,
-        country: req.body.country,
-        deaths: req.body.deaths,
-        cases: req.body.cases,
-    });
-
-    // Save Note in the database
-    casee.save()
-    .then(data => {
-        res.send(data);
+// Retrieve and return all notes from the database.
+exports.findAll = (req, res) => {
+    Case.find()
+    .then(cases => {
+        res.send(cases);
     }).catch(err => {
         res.status(500).send({
-            message: err.message || "Some error occurred while creating the Note."
+            message: err.message || "Some error occurred while retrieving notes."
         });
     });
 };
+
+// Find a single note with a noteId
